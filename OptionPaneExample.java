bbc5ef9dfb0128354487b35772e2e7ac5ac6f76d
Thiago Cuter dos Santos <thiago@ribergrafica.com.br>
2023-07-09 18:20:01 -0300

    Add files via upload

---
+++ OptionPaneExample.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Acer
+ *
+ */
+
+import javax.swing.*;
+
+public class OptionPaneExample {
+
+	/**
+	 * @param args
+	 */
+	JFrame f;
+	
+	OptionPaneExample() {
+	f=new JFrame();
+	JOptionPane.showMessageDialog(f, "Hello World");
+}
+public static void main(String[] args) {	
+	new OptionPaneExample();
+}
+}
+
