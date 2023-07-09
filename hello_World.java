a8dd46d677e15594949d55b0ce90ddf1bc3191c0
Thiago Cuter dos Santos <thiago@ribergrafica.com.br>
2023-07-09 18:32:51 -0300

    Add files via upload

---
+++ hello_World.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package hello_World;
+
+/**
+ * @author Acer
+ *
+ */
+public class hello_World {
+
+	/**
+	 *1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+
+Solução da pelo site: https://www.w3resource.com/java-exercises/basic/index.php
+
+public class Exercise1 {
+ 
+ public static void main(String[] args) {
+  System.out.println("Hello\nAlexandra Abramov!");
+ }
+ 
+}
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello\n Thiago");
+	}
+
+}
